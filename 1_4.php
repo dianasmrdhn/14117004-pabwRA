97826989d9c9828e9865c1e78c9233fa4c8dbd1f
Dian Asmara Dahana <dianasmaradahana27@gmail.com>
2019-10-08 00:21:49 -0700

    prak 6

---
+++ 1_4.php
@@ -0,0 +1,13 @@
+<?php
+$a = 5;
+switch ($a) {
+    case 0;
+        echo '$a sama dengan 0';
+        break;
+    case 1;
+        echo '$a sama dengan 1';
+        break;
+    case 2;
+        echo '$a sama dengan 2';   
+}
+?>
